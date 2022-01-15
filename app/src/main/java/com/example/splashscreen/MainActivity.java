cd661526fe4e2de1b6f302b0259521d9b0f5c07d
Trần Nguyễn Vĩnh Uy <vinhuy2002@gmail.com>
2022-01-15 10:31:13 +0700

    Initial commit

---
+++ app/src/main/java/com/example/splashscreen/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btn;
+    EditText tvInput;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), ScreenB.class);
+                tvInput = findViewById(R.id.tvInput);
+                String str = (String) tvInput.getText().toString();
+                intent.putExtra("str", str);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/splashscreen/ScreenB.java
@@ -0,0 +1,23 @@
+package com.example.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ScreenB extends AppCompatActivity {
+
+
+    TextView tv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_b);
+        tv = findViewById(R.id.tvScreen);
+
+        Bundle bundle = getIntent().getExtras();
+        tv.setText(bundle.getString("str"));
+
+    }
+}
